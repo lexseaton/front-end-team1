849a54ac5b28feac009b4ada1cada864c9285a29
Lex Seaton <lex.seaton@kainos.com>
2024-07-26 09:20:08 +0100

    automated ui tests for view job roles

---
+++ test/ui/viewJobRoleTests.ts
@@ -0,0 +1,35 @@
+import { Builder, Capabilities, WebDriver } from 'selenium-webdriver';
+import webdriver from 'selenium-webdriver';
+import { expect } from 'chai';
+import * as chrome from 'selenium-webdriver/chrome';
+
+describe('Job Roles Test', function () {
+
+    let driver: WebDriver;
+
+    before(async function () {
+        driver = new Builder()
+            .forBrowser('chrome')
+            .setChromeOptions(new chrome.Options())
+            .build();
+
+    });
+
+    // after(async function () {
+    //     try {
+    //         await driver.quit(); // Ensure this is awaited
+    //     } catch (error) {
+    //         console.error('Error quitting the driver:', error);
+    //     }
+    // });
+
+    it('Page should load and title text should display', async function () {
+        const url: string = process.env.UI_TEST_URL || 'http://localhost:3000/openJobRoles';
+        await driver.get(url);
+       // await driver.findElement(By.)
+
+        expect("asda").to.equal('Together we write our story...');
+    });
+
+})
+
