35614b985d5094c10985a7bcd0d1c0d70ea0b2e6
RaggedyRach <rachaelnolan@gmail.com>
2024-07-29 16:44:53 +0100

    chenges line break in HomeController.ts

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import axios
- from "axios";
+import axios from "axios";
+
 axios.defaults.baseURL = process.env.API_URL || 'http://localhost:8080/';
 
 export const URL: string = "/api/openJobRoles/";
