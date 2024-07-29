1ab6c0abf84abd2582c6f6c8b31af18302b4d953
RaggedyRach <rachaelnolan@gmail.com>
2024-07-29 16:43:32 +0100

    first run at passing base url to view

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,4 +1,9 @@
 import express from "express";
+import axios
+ from "axios";
+axios.defaults.baseURL = process.env.API_URL || 'http://localhost:8080/';
+
+export const URL: string = "/api/openJobRoles/";
 
 export const getHomepage = async (req: express.Request, res: express.Response): Promise<void> => {
     res.render('homepage.html');
--- src/controllers/JobRoleController.ts
+++ src/controllers/JobRoleController.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import { getJobRoles } from "../services/JobRoleService";
+import axios, { AxiosResponse } from "axios";
+
+axios.defaults.baseURL = process.env.API_URL || 'http://localhost:8080/';
+
+export const URL: string = "/api/openJobRoles/";
+
 
 export const getAllJobRoles = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
+        
         res.render('openJobRoleList.html', { openJobRoles: await getJobRoles() });
     } catch (e) {
         res.locals.errormessage = e.message;
