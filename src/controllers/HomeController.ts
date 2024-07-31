98598dad4e0c542a1d20c1b9d9b521f239fd849a
RaggedyRach <rachaelnolan@gmail.com>
2024-07-31 12:01:56 +0100

    removing space

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { baseURL } from "..";
 
-
 export const getHomepage = async (req: express.Request, res: express.Response): Promise<void> => {
     res.render('homepage.html', { baseURL });
 }
